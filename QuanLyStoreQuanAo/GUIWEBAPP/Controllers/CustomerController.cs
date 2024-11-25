000427764a15ac4cc3de0c2de3cc1a3e34922ba9
cvluong2003 <cvluong91lh@gmail.com>
2024-11-24 21:04:25 -0500

    update webapp

---
+++ QuanLyStoreQuanAo/GUIWEBAPP/Controllers/CustomerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BUS;
+using DTO;
+namespace GUIWEBAPP.Controllers
+{
+
+    public class CustomerController : Controller
+    {
+        KhachHang_BUS bus = new KhachHang_BUS();
+        QLSHOPQUANAODataContext context = new QLSHOPQUANAODataContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignIn(string username, string password)
+        {
+            if (bus.login(username, password))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+       
+        //public ActionResult Register(FormCollection form)
+        //{
+        //    var khachHang = new KhachHang
+        //    {
+
+        //        HoTen = form["HoTen"],
+        //        Email = form["Email"],
+        //        SoDienThoai = form["SoDienThoai"],
+        //        MatKhau= form["MatKhau"],
+
+        //    };
+
+        //    var confirmPassword = form["confirmpassword"];
+        //    if (confirmPassword != khachHang.MatKhau)
+        //    {
+        //        ModelState.AddModelError("ConfirmPassword", "Mật khẩu không khớp.");
+        //      return View();
+        //    }
+        //   string kq= bus.register(khachHang);
+        //    if (kq == "true")
+        //    {
+        //        return RedirectToAction("Customer", "SignIn");
+        //    }
+        //    else if(kq == "number") 
+        //    {
+        //        ModelState.AddModelError("SoDienThoai", "Số điện thoại không hợp lệ");
+        //        return View();
+        //    }
+        //}
+
+    
+    }
+}
---
+++ QuanLyStoreQuanAo/GUIWEBAPP/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GUIWEBAPP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GUIWEBAPP")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc173e41-1c1d-4cc5-8006-2dc32cb33563")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
